73e4800ddbe0ae295c97bd03c5516e25ad6c9b14
Trevizoli <rtrevizoli@yahoo.com.br>
2026-04-23 22:39:30 -0300

    ES-59: feat(MainStructure) Add backend project main structure

---
+++ src/test/java/com/enersight/EnersightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enersight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnersightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
